7999a9bc76af0bc36ef3bd42f860595e27dd9383
cardon <philantrope1@outlook.com>
2023-01-02 12:16:21 +0100

    add Json

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,21 +2,26 @@ class ArticlesController < ApplicationController
 
   def index
     @articles = Article.all
+    render json: @articles
   end
 
   def new
     @article = Article.new
+    render json: @article
   end
 
   def show
     @article = Article.find(params[:id])
+    render json: @article
   end
 
   def destroy
     @article = Article.destroy(params[:id])
+    render json: @article
   end
 
   def edit
     @article = Article.update(params[:id])
+    render json: @article
   end
 end
